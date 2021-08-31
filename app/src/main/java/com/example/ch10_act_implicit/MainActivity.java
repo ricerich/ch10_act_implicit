978b2bcaae9ed1f831c48b05dc57436887a80695
KJB <drv98@naver.com>
2021-08-31 15:17:31 +0900

    10장 암시적 인텐트 (그림,문자등등)

---
+++ app/src/main/java/com/example/ch10_act_implicit/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.ch10_act_implicit;
+
+import java.io.File;
+
+import android.Manifest;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("암시적 인텐트 예제");
+
+        Button btnDial = (Button) findViewById(R.id.btnDial);
+        Button btnWeb = (Button) findViewById(R.id.btnWeb);
+        Button btnGoogle = (Button) findViewById(R.id.btnGoogle);
+        Button btnSearch = (Button) findViewById(R.id.btnSearch);
+        Button btnSms = (Button) findViewById(R.id.btnSms);
+        Button btnPhoto = (Button) findViewById(R.id.btnPhoto);
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri uri = Uri.parse("tel:010-1234-5678");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.hanb.co.kr");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://maps.google.com/maps?q="
+                        + 37.554264 + "," + 126.913598);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY, "안드로이드");
+                startActivity(intent);
+            }
+        });
+
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.putExtra("sms_body", "안녕하세요");
+                intent.setData(Uri.parse("smsto:" + Uri.encode("010-1234-4567")));
+                startActivity(intent);
+            }
+        });
+
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+//                Intent intent = new Intent(MediaStore. ACTION_IMAGE_CAPTURE);
+//                startActivity(intent);
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = Uri.fromFile(
+                            new File(
+                        Environment.getExternalStorageDirectory().getPath()
+                                +"/Download"
+                                +"/jeju13.jpg"));
+                intent.setDataAndType(uri, "image/jpeg");
+                startActivity(intent);
+            }
+        });
+    }
+
+}
